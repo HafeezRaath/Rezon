087f7b7c8cba6894f8f300ebc1ccace0601a428a
Muhammad Hafeez <hafeezraath806@gmail.com>
2026-05-02 20:26:59 +0500

    fix: resolve LocationDropdown reference error in Navbar and allads

--- Client/src/Components/allads.jsx
+++ Client/src/Components/allads.jsx
@@ -10,6 +10,7 @@ import {
     FaChevronLeft, FaChevronRight, FaCircle, FaSpinner
 } from "react-icons/fa";
 import toast from "react-hot-toast";
+import LocationDropdown from "./LocationDropdown";
 
 
 // 🔧 FIXED: API URL without space
